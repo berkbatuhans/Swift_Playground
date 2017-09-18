3a23efd8a43f6ec91fbf12e3a4d9f45a29a64be9
ST3 <st3@ST3-iMac.local>
2017-09-18 12:39:41 +0300

    initial commit

---
+++ berkbatuhan_TestPlayground.playground/Contents.swift
@@ -0,0 +1,77 @@
+//: Playground - noun: a place where people can play
+
+import UIKit // UIkit for iOs,
+
+var str = "Hello, playground"
+var ad = "Berk"
+
+
+ad = "Hakan"
+ad = "Hakan\"in verisi"
+type(of: ad)
+
+
+var name = "Berk"
+name += " ŞAKAR"
+
+let firstname: String = "Berk"
+
+
+var firstName: String = "Berk Batuhan "
+var lastName: String = "ŞAKAR"
+var fullName: String
+fullName = "İsminiz: " + firstName + lastName
+fullName = "İsminiz: \(firstName) \(lastName)"
+
+//var x,y,z: String
+var x = ""
+var y: String!
+var z: String?
+
+y = nil
+
+print("Merhaba")
+print( x.isEmpty ? "Boş" : "Dolu"  )
+print(fullName.characters.count)
+var smile: Character = "!"
+print(fullName.capitalized)
+print(fullName.uppercased())
+
+var int1: Int
+print(UInt.max)
+print(Int.max)
+
+int1 = 0
+
+typealias Age = UInt16
+
+var birthDate: Age = 1900
+
+var flo1: Float = 1.12
+var dbl: Double = 1.12
+
+var price: Float = 10.101
+type(of: price)
+
+let flt1: Int
+flt1 = 1
+
+
+
+var int2:Int = 10
+var flt2: Float = 1.1
+var dbl2: Double = 11111.11111111
+print(Float(int2) + flt2)
+
+print(dbl2 + Double(flt2))
+
+var number1 = 5
+var number2 = 3
+
+print(number1 == number2)
+print(number1 != number2)
+print(number1 > number2)
+print(number1 < number2)
+print(number1 >= number2)
+print(number1 <= number2)
+
